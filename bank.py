69913d81e32991f2565a2056f842a6a835b50b00
Jose Ortega <jose.ort98@gmail.com>
2022-05-19 13:39:55 -0500

    Entrenamiento data bank

--- bank.py
+++ bank.py
@@ -28,3 +28,35 @@ data.y.replace(['no', 'yes'], [0, 1], inplace = True)
 data.month.replace(['may', 'jun', 'jul', 'aug', 'oct', 'nov', 'dec', 'jan', 'feb','mar', 'apr', 'sep'], [5, 6, 7, 8, 10, 11, 12, 1, 2, 3, 4, 9], inplace = True)
 data.poutcome.replace(['unknown', 'failure', 'other', 'success'], [0, 1, 2, 3], inplace = True)
 data.dropna(axis=0, how='any', inplace=True)
+
+def entrenamiento(model, x_train, x_test, y_train, y_test):
+    kfold = KFold(n_splits=10)
+    cvscores = []
+    for train, test in kfold.split(x_train, y_train):
+        model.fit(x_train[train], y_train[train])
+        scores = model.score(x_train[test], y_train[test])
+        cvscores.append(scores)
+    y_pred = model.predict(x_test)
+    accuracy_validation = np.mean(cvscores)
+    accuracy_test = accuracy_score(y_pred, y_test)
+    return model, accuracy_validation, accuracy_test, y_pred
+
+def metrics(str_model, acc_validation, acc_test, y_test, y_pred):
+    print('-' * 50 + '\n')
+    print(str.upper(str_model))
+    print('\n')
+    print(f'Accuracy de validación: {acc_validation} ')
+    print(f'Accuracy de test: {acc_test} ')
+    print(classification_report(y_test, y_pred))
+    print(f'Y real: {y_test}')
+    print(f'Y predicho: {y_pred}')
+
+
+
+def matriz_confusion(y_test, y_pred):
+    matriz_confusion = confusion_matrix(y_test, y_pred)
+    return matriz_confusion
+
+x = np.array(data.drop(['y'], 1))
+y = np.array(data.y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
